bc25ed484f6425959ad3b400f009ed43273a91c1
Chris Little <Chris@JA.local>
2018-07-26 13:49:02 -0600

    header1

--- header-child.php
+++ header-child.php
@@ -1,5 +1,13 @@
-<html>
+?><!doctype html>
+<html <?php language_attributes(); ?>>
 <head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no">
 <meta name="google-site-verification" content="IbqnUkV-zbQhiegIGzQJvFhkFh_axltYnxwMIPXiAvo" />
-<title>Jake's Archery</title>
-</head> 
+<link rel="profile" href="http://gmpg.org/xfn/11">
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+
+<?php wp_head(); ?>
+</head>
+</html>
+
