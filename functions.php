51151ed7a5f35bc61c90d4502e03017fcbcbf5dc
Chris Little <Chris@JA.local>
2018-08-24 10:25:45 -0600

    breadcrumbs

--- functions.php
+++ functions.php
@@ -1,15 +1,23 @@
 <?php
-function my_theme_enqueue_styles() {
+function my_theme_enqueue_styles()
+{
 
     $parent_style = 'storefront-style';
 
-    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' , '/style-rtl.css' );
-    wp_enqueue_style( 'child-style',
+    wp_enqueue_style($parent_style, get_template_directory_uri() . '/style.css', '/style-rtl.css');
+    wp_enqueue_style(
+        'child-style',
         get_stylesheet_directory_uri() . '/style.css',
-        array( $parent_style ),
+        array($parent_style),
         wp_get_theme()->get('Version')
     );
 }
-add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
-?>
+add_action('wp_enqueue_scripts', 'my_theme_enqueue_styles');
 
+
+add_action( 'init', 'woo_remove_wc_breadcrumbs' );
+function woo_remove_wc_breadcrumbs() {
+    remove_action( 'woocommerce_before_main_content', 'woocommerce_breadcrumb', 20, 0 );
+}
+
+?>
